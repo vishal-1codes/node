1185acdc716d7aa593033230ede653bb0a5386bd
vishalant23 <vishal.pawar@antllp.com>
2024-03-21 18:37:04 +0530

    add connection string

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose')
-
+const uri = process.env.MONGODB_CONNECT_URL || 'mongodb+srv://vishalpawar:tDvvhVIy8i225xKn@own-data.unin18g.mongodb.net/?retryWrites=true&w=majority&appName=own-data';
 const connectDB=async()=>{
     try{
-        await mongoose.connect('mongodb+srv://vishalpawar:lx0V4Q4EWJRMXJTU@own-data.unin18g.mongodb.net/?retryWrites=true&w=majority&appName=own-data?directConnection=true',{
+        await mongoose.connect(uri,{
             useNewUrlParser:true,
             useUnifiedTopology:true
         });
