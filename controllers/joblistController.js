30d36d6f2bd172375e80b61ce691b2c3f7761d21
vishalant23 <vishal.pawar@antllp.com>
2024-03-22 09:41:13 +0530

    update, add joblist data

--- controllers/joblistController.js
+++ controllers/joblistController.js
@@ -10,6 +10,36 @@ const createJoblist=async(req,res)=>{
     }
 };
 
+const getJoblist=async(req,res)=>{
+    try{
+        const joblist=await Joblist.find()
+        res.status(201).json(joblist)
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+};
+
+const deleteJoblist=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const joblist=await Joblist.findByIdAndDelete({_id:id})
+        res.status(201).json(joblist)
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+};
+
+const updateJoblist=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const joblistUpdateDate=req.body
+        const joblist=await Joblist.findByIdAndUpdate({_id:id},joblistUpdateDate,{ new: true })
+        res.status(201).json(joblist)
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+};
+
 module.exports={
-    createJoblist
+    createJoblist,getJoblist,deleteJoblist,updateJoblist
 }
